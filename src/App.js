6cef4213a00eb312f2f0fe229f9fd649b2d2fb1e
component <letrungnghi200k@gmail.com>
2020-10-03 23:55:54 +0700

    baitap

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+// import StateLessComponent from "./components/StateLessComponent";
+// import StateFull, { StateFullComponent } from "./components/StateFullComponent";
+// import Home from "./baiTapChiaComponent/Home";
+// import DemoDataBinding from "./DataBinding/DemoDataBinding";
+// import FuntioEvents from "./HandleEvents/FuntioEvents";
+// import ClassEvent from "./HandleEvents/ClassEvent";
+// import DemoConditionalAndState from "./DemoLogin/DemoConditionalAndState";
+import BaiTapChonMauXe from "./BaiTapChonMauXe";
+function App() {
+  return (
+    <div>
+      {/* <StateLessComponent />
+      <StateFull /> */}
+      {/* <Home /> */}
+      {/* <DemoDataBinding />
+      <FuntioEvents />
+      <ClassEvent/> */}
+      {/* <DemoConditionalAndState/> */}
+      <BaiTapChonMauXe/>
+    </div>
+  );
+}
+
+export default App;
